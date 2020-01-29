94fb7c2dad68a83fcbb4d3f4b55109e853f60f9a
eridhobffry <eridhobffry@github.com>
2020-01-29 12:30:26 +0100

    Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "../reducers";
 
 export const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
   const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? compose(
+          applyMiddleware(sagaMiddleware),
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+      : applyMiddleware(sagaMiddleware)
   );
+
+  sagaMiddleware.run(rootSaga);
   return store;
 };
